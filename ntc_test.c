9c994668b0b7168937ea21525bbbc3bf60111788
Daniel Mather <daniel.mather@gmail.com>
2015-05-02 23:20:37 -0600

    Messing around a bit.

--- ntc_test.c
+++ ntc_test.c
@@ -11,6 +11,8 @@
 
 // Config Bits
 #pragma config CPUDIV = NOCLKDIV
+#pragma config PLLSEL = PLL4X
+#pragma config PCLKEN = ON
 #pragma config FOSC = INTOSCIO
 #pragma config WDTEN = OFF
 #pragma config MCLRE = ON
@@ -45,13 +47,15 @@ void configure_pwm(void)
     CCP1CON = 0b00001100;
     //CCP1CONbits.CCP1M2 = 1;
     //CCP1CONbits.CCP1M3 = 1;
-    T2CON = 0b00000111;
-    PR2 = 249;
+    T2CON = 0b00000100;
+    PR2 = 255;
     CCPR1L = 25;
 }
 
 void main(void)
 {
+    // OSCCON Set main clock to 16MHz
+    OSCCON = 0b01111100;
     float temp = 0;
     // Set all pins RD1-7 to inputs, RD0 (MSB is set to output)
     //TRISD = 0b11111110;
@@ -63,20 +67,23 @@ void main(void)
         // Get temperature on pin RA0
         temp = temperature_get(0);
         if(temp >= 20 && temp <= 22)
-            CCPR1L = 0;
-        else if(temp >= 22 && temp <= 24)
-            CCPR1L = 25;
+            CCPR1L = 180;
+        else if(temp >= 22 && temp <= 24) {
+            CCPR1L = 185;
+            DC1B0 = 1;
+            DC1B1 = 1;
+        }
         else if(temp >= 24 && temp <= 26)
-            CCPR1L = 50;
+            CCPR1L = 190;
         else if(temp >= 26 && temp <= 28)
-            CCPR1L = 100;
+            CCPR1L = 195;
         else if(temp >= 28 && temp <= 30)
-            CCPR1L = 150;
-        else if(temp >= 30 && temp <= 32)
             CCPR1L = 200;
+        else if(temp >= 30 && temp <= 32)
+            CCPR1L = 205;
         else if(temp >= 32 && temp <= 34)
-            CCPR1L = 225;
+            CCPR1L = 235;
         else
-            CCPR1L = 249;
+            CCPR1L = 255;
     }
 }
